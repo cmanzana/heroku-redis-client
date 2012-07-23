490c724131e4681ce5cb0c6ff6df09c62ce55df9
Carlos Manzanares <carlos.manzanares+bitbucket@gmail.com>
2012-07-23 14:43:06 +0300

    moving to expresso

--- test/basic.js
+++ test/basic.js
@@ -1,16 +1,41 @@
-var tap = require('tap');
-var redis = require('../index');
+var assert = require('assert'),
+    sys = require('sys'),
+    redis = require('../index');
 
-tap.test('load works', function (t) {
-    var redisClient = redis.createClient();
 
-    t.ok(redisClient, "local client created");
-    t.equal(redisClient.port, 6379);
-    t.equal(redisClient.host, "127.0.0.1");
 
-    t.ok(redis.redis, "redis object available");
+module.exports = {
+  'should load locally': function() {
+      var redisClient = redis.createClient();
 
-    t.end();
+      assert.isNotNull(redisClient);
+      assert.eql(redisClient.port, 6379);
+      assert.eql(redisClient.host, "127.0.0.1");
+      assert.isNotNull(redis.redis);
 
-    redisClient.quit();
-});
+      redisClient.quit();
+  },
+
+  'should load when in heroku': function() {
+      var currentREDISTOGO_URL = process.env.REDISTOGO_URL;
+      process.env.REDISTOGO_URL = "redis://user:password@chubb.redistogo.com:9332/";
+
+
+      var redisClient = redis.createClient();
+      redisClient.on('error', function (err) {
+          assert.includes(err.toString(), 'invalid password');
+          redisClient.quit();
+          process.exit(0); // this is not great, but I have not found any other way to stop the test suite...
+      });
+
+      assert.isNotNull(redisClient);
+      assert.eql(redisClient.port, 9332);
+      assert.eql(redisClient.host, "chubb.redistogo.com");
+      assert.eql(redisClient.auth_pass, "password");
+      assert.isNotNull(redis.redis);
+
+      redisClient.quit();
+
+      process.env.REDISTOGO_URL = currentREDISTOGO_URL;
+  }
+};
