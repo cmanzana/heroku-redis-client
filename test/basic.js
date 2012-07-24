c7fdea8361c2dbc5d5c5e703088084dca2edf7cc
Carlos Manzanares <carlos.manzanares+bitbucket@gmail.com>
2012-07-24 18:36:58 +0300

    moving to mocha

--- test/basic.js
+++ test/basic.js
@@ -1,38 +1,37 @@
 var assert = require('assert'),
     redis = require('../index');
 
-module.exports = {
-  'should load locally': function() {
-      var redisClient = redis.createClient();
+describe('heroku-redis-client', function() {
+    it('should load locally', function() {
+        var redisClient = redis.createClient();
 
-      assert.isNotNull(redisClient);
-      assert.eql(redisClient.port, 6379);
-      assert.eql(redisClient.host, "127.0.0.1");
-      assert.isNotNull(redis.redis);
+        assert.ok(redisClient);
+        assert.equal(redisClient.port, 6379);
+        assert.equal(redisClient.host, "127.0.0.1");
+        assert.ok(redis.redis);
 
-      redisClient.quit();
-  },
+        redisClient.quit();
+    });
 
-  'should load when in heroku': function() {
-      var currentREDISTOGO_URL = process.env.REDISTOGO_URL;
-      process.env.REDISTOGO_URL = "redis://user:password@chubb.redistogo.com:9332/";
+    it('should load when in heroku', function() {
+        var currentREDISTOGO_URL = process.env.REDISTOGO_URL;
+        process.env.REDISTOGO_URL = "redis://user:password@chubb.redistogo.com:9332/";
 
 
-      var redisClient = redis.createClient();
-      redisClient.on('error', function (err) {
-          assert.includes(err.toString(), 'invalid password');
-          redisClient.quit();
-          process.exit(0); // this is not great, but I have not found any other way to stop the test suite...
-      });
+        var redisClient = redis.createClient();
+        redisClient.on('error', function (err) {
+            assert.includes(err.toString(), 'invalid password');
+            redisClient.quit();
+        });
 
-      assert.isNotNull(redisClient);
-      assert.eql(redisClient.port, 9332);
-      assert.eql(redisClient.host, "chubb.redistogo.com");
-      assert.eql(redisClient.auth_pass, "password");
-      assert.isNotNull(redis.redis);
+        assert.ok(redisClient);
+        assert.equal(redisClient.port, 9332);
+        assert.equal(redisClient.host, "chubb.redistogo.com");
+        assert.equal(redisClient.auth_pass, "password");
+        assert.ok(redis.redis);
 
-      redisClient.quit();
+        redisClient.quit();
 
-      process.env.REDISTOGO_URL = currentREDISTOGO_URL;
-  }
-};
+        process.env.REDISTOGO_URL = currentREDISTOGO_URL;
+    });
+})
